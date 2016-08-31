3d85039d7a2b8c64d9a31eddbacdceadf8a198ad
Mateusz K <awokado_zgora@o2.pl>
2016-08-31 22:32:56 +0200

    fight with VieModel

---
+++ DelegationHelper/Model/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// DAL - data access layer
+/// </summary>
+namespace DelegationHelper.Model
+{
+    class Settings
+    {
+        public static Colors Read()
+        {
+            Properties.Settings settings = Properties.Settings.Default;
+            return new Colors(settings.R, settings.G, settings.B);
+        }
+        public static void Save(Colors color)
+        {
+            Properties.Settings settings = Properties.Settings.Default;
+            settings.R = color.R;
+            settings.G = color.G;
+            settings.B = color.B;
+            settings.Save();
+        }
+    }
+}
---
+++ DelegationHelper/ViewModel/ColorEdit.cs
@@ -0,0 +1,58 @@
+﻿using DelegationHelper.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace DelegationHelper.ViewModel
+{
+    public class ColorEdit
+    {
+        private readonly Model.Colors colors = Settings.Read();
+
+        public byte R
+        {
+            get { return colors.R; }
+            set { colors.R = value; }
+        }
+        public byte G
+        {
+            get { return colors.G; }
+            set { colors.G = value; }
+        }
+        public byte B
+        {
+            get { return colors.B; }
+            set { colors.B = value; }
+        }
+
+        public void Save()
+        {
+            Settings.Save(colors);
+        }
+    
+        public Color Color
+        {
+            get { return colors.ToColor(); }
+        }
+    }
+
+    static class MyExtensions
+    {
+        public static Color ToColor(this Model.Colors colors)
+        {
+            return new Color()
+            {
+                A = 255,
+                R = colors.R,
+                G = colors.G,
+                B = colors.B
+
+            };
+        }
+
+
+    }
+}
