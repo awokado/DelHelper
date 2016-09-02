5d6c616ca0ba8a3b537a95e093e996fdd7de19b3
Mateusz K <awokado_zgora@o2.pl>
2016-09-03 01:06:17 +0200

    MVVM imprelentation

---
+++ DelegationHelper/ViewModel/CurrencyDisplay.cs
@@ -0,0 +1,34 @@
+﻿using DelegationHelper.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegationHelper.ViewModel
+{
+    class CurrencyDisplay : INotifyPropertyChanged
+    {
+        private readonly CurrencyTable curTable = NBPTableDownloader.getNBPCurrencyTable();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public List<Currency> currencyTable
+        {
+            get { Console.WriteLine("--1--------------------------------------------------------------");  return curTable.items; }
+        }
+
+        private void onPropertyChanged(params string[] propertyNames)
+        {
+            if(PropertyChanged != null)
+            {
+                foreach(string propertyName in propertyNames)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+        }
+    }
+}
